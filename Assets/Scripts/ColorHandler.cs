2d751a1cda2aa16aaf44f76271b458e52b1ff496
asherjs <jellyvpr@gmail.com>
2021-05-06 04:46:50 +1000

    first homework completed

---
+++ Assets/Scripts/ColorHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().materials[0].color = Color.gray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+	public int count = 10;
+	public GameObject prefab;
+
+	private void Start()
+	{
+		for (int i = 0; i < count; i++)
+		{
+			var instance = Instantiate(prefab) as GameObject;
+			instance.transform.position = GetRandomPosition();
+			instance.transform.localScale = GetRandomScale();
+		}
+	}
+
+	private Vector3 GetRandomScale()
+	{
+		float scale = Random.Range(1.2f, 3f);
+		return new Vector3(scale, scale, scale);
+	}
+
+	private Vector3 GetRandomPosition()
+	{
+		return new Vector3(
+			Random.Range(-40f, 40f),
+			Random.Range(-40f, 40f),
+			Random.Range(-40f, 40f));
+	}
+}
