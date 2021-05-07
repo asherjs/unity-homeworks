c125e41fa1286d35e2f4256474f18f9a005e707e
asherjs <jellyvpr@gmail.com>
2021-05-07 20:59:17 +1000

    add day/night cycle

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
+    public GameObject sun;
+    public Light sunLight;
+
+    [Range(0, 24)]
+    public float time = 12;
+
+    public float secondsPerMinute = 1;
+    [HideInInspector]
+    public float secondsPerHour;
+    [HideInInspector]
+    public float secondsPerDay;
+
+    public float timeMultiplier = 1;
     void Start()
     {
-        GameObject[] lights = GameObject.FindGameObjectsWithTag("StreetLight");
-        foreach (GameObject light in lights)
-        {
-            light.GetComponent<Light>().enabled = true;
-        }
-            
+        sun = gameObject;
+        sunLight = gameObject.GetComponent<Light>();
+
+        secondsPerHour = secondsPerMinute * 60;
+        secondsPerDay = secondsPerHour * 24;
+
+        // GameObject[] lights = GameObject.FindGameObjectsWithTag("StreetLight");
+        // foreach (GameObject light in lights)
+        //{
+        //light.GetComponent<Light>().enabled = true;
+        //}
+
     }
 
     void Update()
     {
-        
+        SunUpdate();
+
+        time += (Time.deltaTime / secondsPerDay) * timeMultiplier;
+
+        if (time >= 24)
+        {
+            time = 0;
+        }
+    }
+
+    public void SunUpdate()
+    {
+        sun.transform.localRotation = Quaternion.Euler(((time / 24) * 360f) - 90, 90, 0);
     }
 }
